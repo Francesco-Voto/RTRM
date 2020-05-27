437ba02790f7d0bd50ac09aba2e257ffe5303829
Francesco-Voto <francesco.voto.8@gmail.com>
2020-05-27 23:48:08 +0200

    Remove SafeAreaView from App

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-  SafeAreaView, StatusBar,
-} from 'react-native';
+import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './src/Store';
 import { CharactersList } from './src/List/View';
@@ -10,9 +8,7 @@ const App: () => React.ReactNode = () => (
   <Provider store={store}>
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <CharactersList />
-      </SafeAreaView>
+      <CharactersList />
     </>
   </Provider>
 );
